d5eb40881ec953301764b3696c373870e4509798
TrackerSL <157577084+TrackerSL@users.noreply.github.com>
2024-01-24 17:07:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Watching STG Network","Watching Console","Watching Players"];
 
 
 let currentIndex = 0;
